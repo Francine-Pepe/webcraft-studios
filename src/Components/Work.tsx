4263d7323e50c0cbe916972bb0d13e690bf41f53
Francine <franmelopepe2@gmail.com>
2024-06-26 10:17:11 +0200

    work component

---
+++ src/Components/Work.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Work() {
+  return <div className="container" id="work">
+    <div className="work-content">
+        <h1>Work</h1>
+      </div>
+  </div>;
+}
+
+export default Work;
