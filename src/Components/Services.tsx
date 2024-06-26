707eaa49efcf86fb47e0113653734096667669e3
Francine <franmelopepe2@gmail.com>
2024-06-26 10:16:59 +0200

    services component

---
+++ src/Components/Services.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Services() {
+  return (
+    <div className="container" id="services">
+      <div className="services-content">
+        <h1>Services</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
