392ea606c6d1bcc1c028fcbb12d86b041cd491bd
Francine <franmelopepe2@gmail.com>
2024-06-26 10:14:56 +0200

    navbar and colors data

---
+++ src/data.tsx
@@ -0,0 +1,57 @@
+export const nav = [
+  {
+    id: "#home",
+    link: "/",
+    name: "home",
+  },
+  {
+    id: "#services",
+    link: "/#services",
+    name: "services",
+  },
+  {
+    id: "#work",
+    link: "/#work",
+    name: "work",
+  },
+  {
+    id: "#about",
+    link: "/#about",
+    name: "about",
+  },
+  {
+    id: "#contact",
+    link: "/#contact",
+    name: "contact",
+  },
+];
+
+export const circleColorYellowGroup = [
+  {
+    id: 0,
+    color: "#CB2C55",
+  },
+];
+
+export const squareColorsYellow = [
+  {
+    id: 0,
+    color: "#BE6988",
+  },
+  {
+    id: 1,
+    color: "#F2ACCE",
+  },
+  {
+    id: 2,
+    color: "#F2C335",
+  },
+  {
+    id: 3,
+    color: "#D9734D",
+  },
+  {
+    id: 4,
+    color: "#DA1C0C",
+  },
+];
