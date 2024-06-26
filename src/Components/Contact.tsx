9c5926aada79279b7efe0146c6be2ac49ecf4b0c
Francine <franmelopepe2@gmail.com>
2024-06-26 10:15:49 +0200

    contact component

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='container' id="contact">
+      <div className="contact-content">
+        <h1>Contact</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
