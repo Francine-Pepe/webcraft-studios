e324263a0a30ec79b13e9025188f3ba090796730
Francine <franmelopepe2@gmail.com>
2024-07-01 12:44:37 +0200

    Drawer and Video components were added to Home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,11 +1,18 @@
-import React from "react";
 import Navbar from "./Navbar";
-
+import Drawer from "./Drawer";
+import Video from "./Props/Video";
+import { homeText } from "../data";
+import Text from "../Components/Props/Text";
 
 function Home() {
   return (
     <section className="container" id="home">
+      <Drawer />
       <Navbar />
+      <Video />
+      <div className="home-text-container">
+        <Text data={homeText} />
+      </div>
     </section>
   );
 }
