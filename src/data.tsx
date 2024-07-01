c018b9b64b65877aa9047042db80e918c333fa24
Francine <franmelopepe2@gmail.com>
2024-07-01 12:39:23 +0200

    new data added (pages colors)

--- src/data.tsx
+++ src/data.tsx
@@ -29,29 +29,142 @@ export const nav = [
 export const circleColorYellowGroup = [
   {
     id: 0,
-    color: "#CB2C55",
+    color: "#cb132d",
   },
 ];
 
 export const squareColorsYellow = [
   {
     id: 0,
-    color: "#BE6988",
+    color: "#830bd2",
+    theme: "yellow-theme",
   },
   {
     id: 1,
-    color: "#F2ACCE",
+    color: "#c30eb0",
+    theme: "yellow-theme",
   },
   {
     id: 2,
-    color: "#F2C335",
+    color: "#cb132d",
+    theme: "yellow-theme",
   },
   {
     id: 3,
-    color: "#D9734D",
+    color: "#da5a14",
+    theme: "yellow-theme",
   },
   {
     id: 4,
-    color: "#DA1C0C",
+    color: "#dab500",
+    theme: "yellow-theme",
+  },
+];
+
+export const squareColorsBlue = [
+  {
+    id: 0,
+    color: "#001051",
+    theme: "blue-theme",
+  },
+  {
+    id: 1,
+    color: "#16095f",
+    theme: "blue-theme",
+  },
+  {
+    id: 2,
+    color: "#4d0666",
+    theme: "blue-theme",
+  },
+  {
+    id: 3,
+    color: "#844c60",
+    theme: "blue-theme",
+  },
+  {
+    id: 4,
+    color: "#ab7e7e",
+    theme: "blue-theme",
+  },
+];
+export const squareColorsGrey = [
+  {
+    id: 0,
+    color: "#9da6d1",
+    theme: "grey-theme",
+  },
+  {
+    id: 1,
+    color: "#accc9b",
+    theme: "grey-theme",
+  },
+  {
+    id: 2,
+    color: "#e4dedf",
+    theme: "grey-theme",
+  },
+  {
+    id: 3,
+    color: "#cfdfff",
+    theme: "grey-theme",
+  },
+  {
+    id: 4,
+    color: "#bccceb",
+    theme: "grey-theme",
+  },
+];
+export const squareColorsGreen = [
+  {
+    id: 0,
+    color: "#b0d8db",
+    theme: "green-theme",
+  },
+  {
+    id: 1,
+    color: "#fcdadc",
+    theme: "green-theme",
+  },
+  {
+    id: 2,
+    color: "#8ac5c8",
+    theme: "green-theme",
+  },
+  {
+    id: 3,
+    color: "#F3E5E4",
+    theme: "green-theme",
+  },
+  {
+    id: 4,
+    color: "#e08a8f",
+    theme: "green-theme",
+  },
+];
+
+export const languages = [
+  {
+    id: 0,
+    name: "en",
+  },
+  {
+    id: 1,
+    name: "de",
+  },
+  {
+    id: 2,
+    name: "pt",
+  },
+  {
+    id: 3,
+    name: "es",
+  },
+];
+
+export const homeText = [
+  {
+    id: 0,
+    text: "we are Webcraft Studios, we work From Concept to Code, Crafting Your Digital Universe.",
   },
 ];
