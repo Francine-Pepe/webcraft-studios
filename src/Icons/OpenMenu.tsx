967293dc3cbf5e7a4b640eadc4eb094e7e647efa
Francine <franmelopepe2@gmail.com>
2024-07-09 13:56:47 +0200

    new classname added to this component

--- src/Icons/OpenMenu.tsx
+++ src/Icons/OpenMenu.tsx
@@ -8,7 +8,7 @@ export function OpenMenu(props: SVGProps<SVGSVGElement>) {
       height="3em"
       viewBox="0 0 24 24"
       {...props}
-      className="menu"
+      className="menu clickable-icon"
     >
       <g
         fill="none"
