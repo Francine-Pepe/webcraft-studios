e0a41b5a0c1e8f76b3aed9c0541ab71e1db260cd
Francine <franmelopepe2@gmail.com>
2024-09-02 16:30:04 +0200

    services component

--- src/Components/Services.tsx
+++ src/Components/Services.tsx
@@ -1,9 +1,17 @@
+import { pageDescriptions, servicesCard } from "../data";
+import Grid from "./Props/Grid";
+import TitleDescriptios from "./Props/TitleDescriptios";
 
 function Services() {
   return (
     <div className="container" id="services">
       <div className="services-content">
-        <h1>Services</h1>
+        <div className="descriptions">
+          <TitleDescriptios data={pageDescriptions} />
+        </div>
+        <div className="grid-row">
+          <Grid data={servicesCard} />
+        </div>
       </div>
     </div>
   );
